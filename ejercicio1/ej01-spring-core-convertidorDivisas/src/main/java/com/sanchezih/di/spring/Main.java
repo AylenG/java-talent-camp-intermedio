4073c7615b828057dd807900cd7d635f11dcdd59
AylenG <aylen.p.gomez@gmail.com>
2022-09-06 22:03:06 -0300

    Solución ejercicio 1

--- ejercicio1/ej01-spring-core-convertidorDivisas/src/main/java/com/sanchezih/di/spring/Main.java
+++ ejercicio1/ej01-spring-core-convertidorDivisas/src/main/java/com/sanchezih/di/spring/Main.java
@@ -1,14 +1,19 @@
 package com.sanchezih.di.spring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.sanchezih.di.spring.service.Converter;
 import com.sanchezih.di.spring.service.impl.ARSToUSDConverter;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		BeanFactory applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
 		double cantidad = 100;
-		Converter c = new ARSToUSDConverter();
+		Converter c = (ARSToUSDConverter) applicationContext.getBean("converterUSD");
 		System.out.println(c.convert(cantidad));
 
 	}
